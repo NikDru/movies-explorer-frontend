bd1b135e477cd210233e3342cfe60b585bb0ba97
Nik Vafin <nik_vafin@mail.ru>
2022-10-21 08:42:40 +0300

    main page fix

--- movies-board/src/components/Footer/Footer.js
+++ movies-board/src/components/Footer/Footer.js
@@ -1,10 +1,14 @@
 function Footer(props) {
   return (
     <footer className='footer'>
-      <h2>Учебный проект Яндекс.Практикум х BeatFilm.</h2>
-      <p>© 2020</p>
-      <p>Яндекс.Практикум</p>
-      <p>Github</p>
+      <h2 className='footer__header'>Учебный проект Яндекс.Практикум х BeatFilm.</h2>
+      <div className='footer__content'>
+        <p className='footer__date'>&copy; 2022</p>
+        <div className='footer__links'>
+          <p className='footer__link'>Яндекс.Практикум</p>
+          <p className='footer__link'>Github</p>
+        </div>
+      </div>
     </footer>
   );
 }
