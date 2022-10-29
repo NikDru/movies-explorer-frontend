4767a4409ddca8e09f9cae4aebfc25428baabf22
Nik Vafin <nik_vafin@mail.ru>
2022-10-29 06:31:12 +0300

    Исправлены замечания по первому ревью

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -3,13 +3,13 @@ import photo from '../../images/card_photo.png';
 function MoviesCard(props) {
   return (
     <div className='card'>
-      <img className='card__photo' src={photo} alt='Movie photo'/>
+      <img className='card__photo' src={photo} alt='Movie thumbnail'/>
       <div className='card__sign'>
         <p className='card__name'>Gimme Danger: История Игги и The Stooges</p>
         {
           true ?
-            <button className='card__delete'></button> :
-            <button className='button-animation card__like'></button>
+            <button type='button' className='card__delete'></button> :
+            <button type='button' className='button-animation card__like'></button>
         }
       </div>
       <p className='card__length'>1ч 42м</p>
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,13 +3,15 @@ import Switcher from '../Switcher/Switcher';
 function SearchForm(props) {
   return (
     <section className='search'>
-      <div className='search__input-container'>
-        <input type='text' className='search__input' placeholder='Фильм'/>
-        <button className='button-animation search__button'>Поиск</button>
-      </div>
-      <div className='search__switcher'>
-        <Switcher />
-      </div>
+      <form className='search__form'>
+        <div className='search__input-container'>
+          <input type='text' className='search__input' placeholder='Фильм' required/>
+          <button type='submit' className='button-animation search__button'>Поиск</button>
+        </div>
+        <div className='search__switcher'>
+          <Switcher />
+        </div>
+      </form>
     </section>
   );
 }
